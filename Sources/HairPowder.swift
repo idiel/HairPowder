42318c054a8bcda18843cc6bf600a71d630f5437
Sean Park <i2workshop@gmail.com>
2017-09-22 17:51:20 +0900

    HairStyle Added

--- Sources/HairPowder.swift
+++ Sources/HairPowder.swift
@@ -11,6 +11,43 @@ import UIKit
 open class HairPowder {
     static let instance = HairPowder()
     
+    var hairStyle: HairStyle = .default
+    
+    public enum HairStyle {
+        case `default`
+        case boxCut
+        
+        func styledHairView(frame: CGRect) -> UIView {
+            switch self {
+            case .default:
+                let powderView = HairPowderView(frame: frame)
+                powderView.backgroundColor = UIColor.clear
+                powderView.clipsToBounds = true
+                return powderView
+            case .boxCut:
+                let powderView = BoxCutView(frame: frame)
+                powderView.backgroundColor = UIColor.clear
+                powderView.clipsToBounds = true
+                return powderView
+        }
+    }
+    
+    private class BoxCutView: UIView {
+        static let cornerY: CGFloat = 35
+        override func draw(_ rect: CGRect)
+        {
+            let width = frame.width > frame.height ? frame.height : frame.width
+            
+            let rectPath = UIBezierPath()
+            rectPath.move(to: CGPoint(x:0, y:0))
+            rectPath.addLine(to: CGPoint(x: width, y: 0))
+            rectPath.addLine(to: CGPoint(x: width, y: BoxCutView.cornerY))
+            rectPath.addLine(to: CGPoint(x: 0, y: BoxCutView.cornerY))
+            rectPath.close()
+            rectPath.fill()
+        }
+    }
+    
     private class HairPowderView: UIView {
         static let cornerRadius: CGFloat = 40
         static let cornerY: CGFloat = 35
@@ -44,27 +81,40 @@ open class HairPowder {
         }
     }
     
+    private var statusWindow: UIWindow?
     
-    private var statusWindow: UIWindow = {
-        let width = UIApplication.shared.keyWindow?.frame.width ?? 0
-        let height = UIApplication.shared.keyWindow?.frame.height ?? 0
+    private func powderedWindow() -> UIWindow {
+        if let existingWindow = self.statusWindow { return existingWindow }
+        
+        let width: CGFloat = window?.frame.width ?? 0
+        let height: CGFloat = window?.frame.height ?? 0
         
         let statusWindow = UIWindow(frame: CGRect(x: 0, y: 0, width: width, height: 0))
         statusWindow.windowLevel = UIWindowLevelStatusBar - 1
         
-        let hairPowderView = HairPowderView(frame: CGRect(x: 0, y: 0, width: width, height: height))
-        hairPowderView.backgroundColor = UIColor.clear
-        hairPowderView.clipsToBounds = true
-        statusWindow.addSubview(hairPowderView)
+        let hairView = hairStyle.styledHairView(frame: CGRect(x: 0, y: 0, width: width, height: height))
+        statusWindow.addSubview(hairView)
+        
+        self.statusWindow = statusWindow
+        
         return statusWindow
-    }()
+    }
+    
+    var window: UIWindow? {
+        if let window = UIApplication.shared.keyWindow {
+            return window
+        } else if let window = UIApplication.shared.windows.first {
+            return window
+        }
+        return nil
+    }
     
     public func spread() {
-        guard let window = UIApplication.shared.keyWindow else { return }
+        guard let window = window else { return }
         if #available(iOS 11.0, *) {
             if window.safeAreaInsets.top > 0.0 {
                 DispatchQueue.main.async { [weak self] in
-                    self?.statusWindow.makeKeyAndVisible()
+                    self?.powderedWindow().makeKeyAndVisible()
                     DispatchQueue.main.async { [weak self] in
                         window.makeKey()
                     }
